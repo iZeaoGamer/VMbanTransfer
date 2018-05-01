84ad4a106643918076dbd9a2270a50b7202931d1
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-05-01 16:39:00 +0100

    Main: Fixed Class.

--- src/VMBanTransfer/Main.php
+++ src/VMBanTransfer/Main.php
@@ -16,7 +16,7 @@ public function onEnable() {
     $this->getLogger()->info("Plugin has been enabled succesfully. looking for errors. If there's no errors, then you're all good.");
 }
     
-public function onLogin(PlayerPreLogin $event) {
+public function onLogin(PlayerPreLoginEvent $event) {
     $player = $event->getPlayer();
    if ($this->isBanned($player));
 $event->getPlayer()->transfer("voidhackerpe.ml", "25640");
